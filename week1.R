555547b8de9e3dea0fe8e02de01aa208d6f9382b
v-kozhevn <vladislav.a.kozhevnikov@gmail.com>
2018-06-17 18:13:54 +0200

    Initial commit

---
+++ week1.R
@@ -0,0 +1,49 @@
+library(readr)
+library(tidyr)
+library(dplyr)
+library(highcharter)
+library(ggplot2)
+library(lubridate)
+
+#loading the data from Worldbank
+
+life_exp <- read_csv("data/life_expectancy.csv", skip = 4)
+countries <- read_csv("data/countries.csv")
+
+
+# Making data clean and long and limiting to 1990 - 2009
+
+life_exp <- life_exp %>%
+  select( -3, -4, -62, -63)
+
+life_exp <- life_exp %>%
+  filter(!is.na(life_exp$`2016`))
+
+life_exp <- life_exp %>%
+  inner_join(countries[1:3], by = "Country Code") 
+
+life_exp <- life_exp %>%
+  filter(!is.na(Region))
+
+life_exp <- life_exp %>%
+  gather(3:59, key = "Year", value = "life_exp_years")
+
+life_exp$Year <- as.numeric(life_exp$Year)
+
+life_exp_1990 <- life_exp %>%
+  filter(between(Year, 1990, 2009))
+
+
+#separate sets for Singapore,  APJ and high-income countries
+
+life_exp_sing <- life_exp_1990 %>%
+  filter(life_exp_1990$`Country Code`== "SGP")
+
+life_exp_apj <- life_exp_1990 %>%
+  filter(Region == "East Asia & Pacific")
+
+life_exp_high_income <- life_exp_1990 %>%
+  filter(IncomeGroup == "High income")
+
+
+
